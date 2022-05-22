728fd33e77152ebd3003fe152283f59a245241ff
nichyjt <nicholas.yek@gmail.com>
2022-05-22 19:48:20 +0800

    Refactor firebase auth saving logic to be more hardcoded

--- internal/backend/auth.go
+++ internal/backend/auth.go
@@ -5,31 +5,73 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
 	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 )
+
+// Programatically recreates the google service account JSON
+// Do this because Github and Heroku do not have a native way of storing JSON secrets
+func GetGoogleCredJson(isProd bool) map[string]string {
+	var projectId string
+	var private_key_id string
+	private_key, _ := os.LookupEnv("FIREBASE_KEY")
+	var client_email string
+	var client_id string
+	var client_x509_cert_url string
+	if isProd {
+		projectId = "findnus-prod"
+		private_key_id = "8c637a14442a5a1848af1da952d859148cfc063c"
+		client_email = "firebase-adminsdk-ssly0@findnus-prod.iam.gserviceaccount.com"
+		client_id = "117830696574462735012"
+		client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ssly0%40findnus-prod.iam.gserviceaccount.com"
+	} else {
+		projectId = "findnus-dev"
+		private_key_id = "0cdf90c387f5d81121bcccb8ba1f9403e77cf2a4"
+		client_email = "firebase-adminsdk-9zxcr@findnus-dev.iam.gserviceaccount.com"
+		client_id = "116839976717740702813"
+		client_x509_cert_url = "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-9zxcr%40findnus-dev.iam.gserviceaccount.com"
+	}
+	return map[string]string{
+		"type":                        "service_account",
+		"project_id":                  projectId,
+		"private_key_id":              private_key_id,
+		"private_key":                 private_key,
+		"client_email":                client_email,
+		"client_id":                   client_id,
+		"auth_uri":                    "https://accounts.google.com/o/oauth2/auth",
+		"token_uri":                   "https://oauth2.googleapis.com/token",
+		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		"client_x509_cert_url":        client_x509_cert_url,
+	}
+}
 
 // Creates an Admin SDK instance for the backend to do Firebase Auth Operations.
 func InitFirebase() firebase.App {
 	var app *firebase.App
 	var err error
+	config := &firebase.Config{}
 	// Check if we are running locally with the existence of secrets folder
 	if _, err := os.Stat("../../secrets"); err == nil {
 		opt := option.WithCredentialsFile("../../secrets/findnus-dev-firebase-adminsdk-9zxcr-0cdf90c387.json")
-		config := &firebase.Config{}
 		app, err = firebase.NewApp(context.Background(), config, opt)
 	} else {
-		// We are in a docker container on UAT/Prod. Env var to service account should exist
-		app, err = firebase.NewApp(context.Background(), nil)
+		// We are in a staged environment (Github || Heroku)
+		// Get our credentials and load up firebase
+		var creds *google.Credentials
+		_, isProd := os.LookupEnv("PRODUCTION")
+		creds.JSON, _ = json.Marshal(GetGoogleCredJson(isProd))
+		opt := option.WithCredentials(creds)
+		app, err = firebase.NewApp(context.Background(), config, opt)
 	}
 	if err != nil {
-		log.Fatalf("Error init firebase, %v\n", err)
+		log.Fatalf("Error init-ing firebase, %v\n", err)
 	}
-	log.Printf("Session ok!")
 	return *app
 }
 
