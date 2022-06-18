f317c927c0ce0c7906287295b299aee11520ddb9
nichyjt <nicholas.yek@gmail.com>
2022-06-18 17:41:22 +0800

    Add RPC MQ logic and refactor routing to main gofile for conciseness

--- internal/item/main.go
+++ internal/item/main.go
@@ -1,9 +1,47 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+// Recieves work to be done
+func HandleRequest(d amqp.Delivery) {
+	msg := UnmarshallMessage(d.Body)
+	switch msg.Operation_type {
+	case OPERATION_NEW_ITEM:
+		DoAddItem(msg)
+		break
+	case OPERATION_PATCH_ITEM:
+		DoUpdateItem(msg)
+		break
+	case OPERATION_GET_ITEM:
+		item := DoGetItem(msg)
+		PublishResponse(item, d)
+		break
+	case OPERATION_GET_ITEM_LIST:
+		//foo
+		fmt.Println("Get Item List Triggered")
+		items := DoGetManyItems(msg)
+		PublishResponse(items, d)
+		break
+	case OPERATION_DEL_ITEM:
+		DoDeleteItem(msg)
+		break
+	default:
+		// foo
+		break
+	}
+}
+
 // Item microservice entrypoint
 func main() {
 	SetupMongo()
 	SetupMessageBrokerConnection()
-	SetupChannelQueues()
-	ConsumeMessages()
+	SetupChannel()
+	go ConsumeMessages()
+	go ConsumeGetMessages()
+	forever := make(chan bool)
+	<-forever
 }
