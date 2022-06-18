b83edbe9a17b97f4a61524c2925bd754e5b4ee5b
nichyjt <nicholas.yek@gmail.com>
2022-06-18 17:49:04 +0800

    Add getitem handlers and testing logic

---
+++ internal/backend/getitem.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+/* ---- ITEM RETRIEVAL ---- */
+func HandleGetOneItem(c *gin.Context) {
+	params := GetParams(c)
+	// Ensure that required parameters exist
+	if _, ok := params["Id"]; !ok {
+		c.JSON(400, gin.H{
+			"message": "Missing Id parameter",
+		})
+		return
+	}
+	msg := PrepareMessage(params, nil, OPERATION_GET_ITEM)
+	id := GetJobId()
+	PublishGetItemMessage(ItemChannel, msg, id)
+	res := PollResponse(id)
+	if res == nil {
+		c.JSON(500, gin.H{
+			"message": "No response internally after 10s",
+		})
+		return
+	}
+	var generic map[string]interface{}
+	json.Unmarshal(res, &generic)
+	item := ParseGetOneItemRPC(generic)
+	c.JSON(200, item)
+}
+
+// Filter peek
+func HandleGetManyItems(c *gin.Context) {
+	params := GetParams(c)
+	msg := PrepareMessage(params, nil, OPERATION_GET_ITEM_LIST)
+	id := GetJobId()
+	PublishGetItemMessage(ItemChannel, msg, id)
+	res := PollResponse(id)
+	if res == nil {
+		c.JSON(500, gin.H{
+			"message": "No response internally after 10s",
+		})
+		return
+	}
+	item := ParseGetManyItemsRPC(res)
+	c.JSON(200, item)
+}
+
+func ParseGetOneItemRPC(tmp map[string]interface{}) Item {
+	// var tmp map[string]interface{}
+	var res Item
+	// json.Unmarshal(data, &tmp)
+
+	// Transform required maps
+	tmp["Category"] = GetCategoryString(tmp["Category"].(float64))
+
+	// Transform optional maps
+	if val, ok := tmp["Contact_method"]; ok {
+		tmp["Contact_method"] = GetContactString(val.(float64))
+	}
+
+	// TODO: See if we can implement a custom unmarshaller that handles re-mapping of fields..
+	// ..directly rather than having to marshal and unmarshal which is inefficient
+	raw, _ := json.Marshal(tmp)
+	json.Unmarshal(raw, &res)
+	return res
+}
+
+func ParseGetManyItemsRPC(data []byte) []Item {
+	var tmp []map[string]interface{} // element of unmarshalled items
+	var res []Item
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for _, elem := range tmp {
+		item := ParseGetOneItemRPC(elem)
+		res = append(res, item)
+	}
+	return res
+}
