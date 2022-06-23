424d4a29d5d3c36ec982a67e1755e574db4616c9
nichyjt <nicholas.yek@gmail.com>
2022-06-23 22:09:53 +0800

    Add elasticsearch message handler

--- internal/item/main.go
+++ internal/item/main.go
@@ -1,45 +1,95 @@
 package main
 
 import (
+	"log"
+
 	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// Recieves work to be done
+// Recieves work from MessageQueue to execute
 func HandleRequest(d amqp.Delivery) {
 	msg := UnmarshallMessage(d.Body)
 	switch msg.Operation_type {
 	case OPERATION_NEW_ITEM:
-		DoAddItem(msg)
+		collection, objId := DoAddItem(msg)
+		if objId == primitive.NilObjectID {
+			log.Println("Error parsing DoAddItem - Aborting Es Add")
+			return
+		}
+		if collection == COLL_FOUND {
+			esItem := ElasticParseBody(msg.Body, objId)
+			if esItem != (ElasticItem{}) {
+				ElasticAddItem(esItem)
+			}
+		}
 		break
+
 	case OPERATION_PATCH_ITEM:
-		DoUpdateItem(msg)
+		id, err := DoUpdateItem(msg)
+		if err != nil {
+			log.Println("Error patching item on MongoDB:", err.Error())
+			return
+		}
+		// Check if patched item should be registered on Es
+		if id != "" {
+			objId, err := primitive.ObjectIDFromHex(id)
+			if err != nil {
+				log.Println("Error patching item to EsCloud:", err.Error())
+				return
+			}
+			esItem := ElasticParseBody(msg.Body, objId)
+			ElasticUpdateItem(esItem)
+		}
 		break
+
 	case OPERATION_GET_ITEM:
 		item := DoGetItem(msg)
 		PublishResponse(item, d)
 		break
+
 	case OPERATION_GET_ITEM_LIST:
 		items := DoGetManyItems(msg)
 		PublishResponse(items, d)
 		break
+
 	case OPERATION_DEL_ITEM:
-		DoDeleteItem(msg)
+		id, err := DoDeleteItem(msg)
+		if err != nil {
+			log.Println("Error deleting item on MongoDB:", err.Error())
+			return
+		}
+		if id != "" {
+			ElasticDeleteItem(id)
+		}
 		break
+
+	case OPERATION_SEARCH:
+		qry := GetElasticQuery(msg.Params)
+		// Safety catch to prevent ALL the items from being returned
+		if qry == "" {
+			foo := []ElasticItem{}
+			PublishResponse(foo, d)
+			return
+		}
+		res := ElasticSearchGeneral(qry)
+		PublishResponse(res, d)
+
 	default:
-		// foo
+		// Should not reach here -- do nothing
 		break
 	}
 }
 
 // Item microservice entrypoint
 func main() {
-	// SetupMongo()
-	// SetupMessageBrokerConnection()
-	// SetupChannel()
-	// go ConsumeMessages()
-	// go ConsumeGetMessages()
-	// forever := make(chan bool)
-	// <-forever
 	SetupElasticClient()
 	ElasticInitIndex()
+	SetupMongo()
+	SetupMessageBrokerConnection()
+	SetupChannel()
+	go ConsumeMessages()
+	go ConsumeGetMessages()
+	forever := make(chan bool)
+	<-forever
 }
