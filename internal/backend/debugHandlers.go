4c25ce5ddd1684c1322a0da70944be78efa85645
nichyjt <nicholas.yek@gmail.com>
2022-12-12 13:44:07 +0800

    Update migration workflows and add triggers

--- internal/backend/debugHandlers.go
+++ internal/backend/debugHandlers.go
@@ -5,14 +5,11 @@ import (
 )
 
 // Pings back the requester. Used to show that backend container is alive.
-// Also tests MQ and database functions to 'keep-alive' the connections
 func debugPingHandler(c *gin.Context) {
 	// Format response
 	c.JSON(200, gin.H{
 		"message": "Hello! You have reached FindNUS.",
 	})
-	// Additional wake-up functionality
-	debugGetDemoItem()
 }
 
 // User passed the AuthGuard. User is logged in, hence authenticated to do priviledged operations.
